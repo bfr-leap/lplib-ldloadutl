45e2a493fbc10cbd7d2099ac62416e869d3c5459
unknown <arturo.mayorga@protonmail.com>
2024-04-06 01:01:03 -0400

    adding pdssrc data loader

---
+++ src/ldata-loaders/ldata-pdcsrc-data-loader.ts
@@ -0,0 +1,28 @@
+import { ldataWriteFile, ldataReadFile } from './fsutil';
+import { PodcastScriptedSrc } from 'ir-endpoints-types';
+
+
+const MNT_PT = './public/data/ldata-pdcsrc/';
+
+export function getSimsessionPodcastScriptedSrc(
+    subsessionId: number,
+    simsessionNumber: number
+): PodcastScriptedSrc | null {
+    return ldataReadFile<PodcastScriptedSrc>(
+        MNT_PT,
+        'simsessionPodcastScriptedSrc',
+        [subsessionId, simsessionNumber]
+    );
+}
+
+export function saveSimsessionSummary(
+    subsessionId: number,
+    simsessionNumber: number,
+    dataset: PodcastScriptedSrc
+): void {
+    ldataWriteFile(dataset, MNT_PT, 'simsessionPodcastScriptedSrc', [
+        subsessionId,
+        simsessionNumber,
+    ]);
+}
+
